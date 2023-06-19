33a0d38918e5107e25c368dfcc5cb15c289f7811
Raheem Carridice <carridiceraheem871@gmail.com>
2023-06-19 08:46:39 -0500

    added express and public folder

--- start.js
+++
@@ -1,8 +0,0 @@
-// const startbtn = document.querySelector('btn-start');
-// const contents = document.querySelector('.content');
-
-// //hidding the contents on game page
-// startbtn.addEventListener('click', hideContents)
-// function hideContents(){
-//     contents.classList.add('hide-contents')
-// }
